451327d1972068ea9ab2534d099c5a26a4444b81
LsyralliH <133071553+LsyralliH@users.noreply.github.com>
2025-11-21 12:40:45 -0300

    Create CodigoFonte.java

---
+++ CodigoFonte.java
@@ -0,0 +1,13 @@
+public class CodigoFonte {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        int c = a + b;
+
+        if (c > 20) {
+            System.out.println("Maior que 20");
+        } else {
+            System.out.println("Menor ou igual a 20");
+        }
+    }   
+}
